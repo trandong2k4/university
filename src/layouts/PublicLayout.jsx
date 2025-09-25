e4e31e5046c7e1ecdfe91f4654368bd1b6cd4e37
Trần Đồng <nguyendong20041@gmail.com>
2025-09-25 15:17:45 +0700

    Update PublicLayout.jsx

--- src/layouts/PublicLayout.jsx
+++ src/layouts/PublicLayout.jsx
@@ -4,13 +4,16 @@ import HeaderPublic from "../components/HeaderPublic";
 import Footer from "../components/Footer";
 import Chatbot from "../components/Chatbot";
 import "../styles/public-layout.css"; // thêm CSS riêng
+import imglearning from "/src/assets/learning.jpg";
 
 export default function PublicLayout() {
   return (
     <div className="public-layout">
 
       <HeaderPublic />
-      <img class="img-all" src="/src/assets/learning.jpg" alt=""></img>
+      <div className="img-all">
+        <img src={imglearning} alt="Errol" />
+      </div>
       <main className="public-layout-content">
         <Outlet /> {/* Render các trang public */}
       </main>
